c64f6aa161e8e2525f73c269686f78530d72628a
Maverick <murrayduke@rogers.com>
2019-11-24 20:10:04 -0500

    Final update before SQLite is added

---
+++ CoffeeRun/CoffeeRun/Models/CurrentOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeRun.Models
+{
+    public class CurrentOrder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CoffeeSize { get; set; }
+        public string CoffeeType { get; set; }
+        public bool Paid { get; set; }
+    }
+}
