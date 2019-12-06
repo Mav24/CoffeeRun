87bbddd6d04a6a23cb6b4179118e40adf014045f
Maverick <murrayduke@rogers.com>
2019-12-05 23:08:36 -0500

    Update before adding admob

--- CoffeeRun/CoffeeRun.Android/MainActivity.cs
+++ CoffeeRun/CoffeeRun.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CoffeeRun.Droid
 {
-    [Activity(Label = "CoffeeRun", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CoffeeRun", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
