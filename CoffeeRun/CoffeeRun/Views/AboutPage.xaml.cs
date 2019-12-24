f42ac7da116d59b6ead332f9d6df75479f37efbf
Maverick <murrayduke@rogers.com>
2019-12-24 12:59:09 -0500

    UI Updates, Set up Rating app on about page

--- CoffeeRun/CoffeeRun/Views/AboutPage.xaml.cs
+++ CoffeeRun/CoffeeRun/Views/AboutPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace CoffeeRun.Views
 {
@@ -15,6 +16,11 @@ namespace CoffeeRun.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void ReviewApp_Clicked(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync(new Uri($"market://details?id={AppInfo.PackageName}"));
         }
     }
 }
